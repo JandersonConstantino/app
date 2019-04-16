223f1dfc5c3c13b54cf429b739133c7e138af062
JandersonConstantino <jandersonconstantino@gmail.com>
2019-04-15 22:04:07 -0300

    jest + enzyme configuration

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
+import * as React from 'react'
 import {
   Text,
   TouchableOpacity,
-  View,
   StyleSheet,
 } from 'react-native'
-import { Ionicons } from '@expo/vector-icons'
 
 export type Option = {
   checked: boolean
@@ -13,11 +11,11 @@ export type Option = {
   label: string
 }
 
-type Props = {
+export type Props = {
   options: Option[]
 }
 
-type State = {
+export type State = {
   checkboxes: Option[]
 }
 
@@ -43,19 +41,14 @@ export class Checkbox extends React.Component<Props, State> {
     const { checkboxes } = this.state
 
     return (
-      <View>
+      <>
         {checkboxes.map(({ key, label, checked }) => (
-          <TouchableOpacity
-            onPress={() => this.setChecked(key)} key={key}
-            style={styles.touchable}
-          >
-            <Ionicons size={32} color='black'
-              name={checked ? 'md-checkmark-circle' : 'md-radio-button-off'}
-            />
-            <Text style={styles.label}>{label}</Text>
+          <TouchableOpacity onPress={() => this.setChecked(key)} key={key}>
+            <Text>{checked ? '[X]' : '[ ]'}</Text>
+            <Text>{label}</Text>
           </TouchableOpacity>
         ))}
-      </View>
+      </>
     )
   }
 }
--- src/components/Checkbox/__tests__/Checkbox.test.tsx
+++ src/components/Checkbox/__tests__/Checkbox.test.tsx
@@ -1,3 +1,19 @@
-it('simple test', () => {
-  expect(1).toBe(1)
+import * as React from "react"
+import { shallow } from "enzyme"
+
+import { Checkbox, Props, State } from "../Checkbox"
+
+const props: Props = {
+  options: [
+    { checked: false, key: "1", label: "One" },
+    { checked: false, key: "2", label: "Two" }
+  ]
+}
+
+const checkbox = shallow<Props, State>(<Checkbox {...props} />)
+
+describe("Checkbox", () => {
+  it("renders correctly", () => {
+    expect(checkbox).toMatchSnapshot()
+  })
 })
